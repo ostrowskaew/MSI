601cdea6dc40904743ce40618c8023630d051d94
ostrowskaew <ostrowskaew@gmail.com>
2021-05-16 12:44:42 +0200

    login responisve

--- MSI-project/src/app/app-routing.module.ts
+++ MSI-project/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   { path: 'owner-equipement', loadChildren: () => import('./owner.module').then(m => m.OwnerModule) },
   { path: 'home', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: AuthComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
